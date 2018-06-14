695d32784cf096aed7431c592aa3c0bb999179c6
Martin Berthold <martin.berthold@akquinet.de>
2018-06-14 09:21:17 +0200

    First version

---
+++ src/qlik-engineapi-generator/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace QlikApiParser
+{
+    #region Usings
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Xml;
+    using Microsoft.Extensions.PlatformAbstractions;
+    using Newtonsoft.Json;
+    using NLog;
+    using NLog.Config;
+    #endregion
+
+    class Program
+    {
+        #region Logger
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        #endregion
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                SetLoggerSettings();
+                var workingFile = args?.FirstOrDefault() ?? null;
+                if (workingFile == null)
+                {
+                    logger.Warn("No working file found.");
+                    return;
+                }
+                logger.Info("Start parsing...");
+                var qlikApiReader = new QlikApiReader();
+                qlikApiReader.Parse(workingFile);
+                logger.Info("Finish");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "APP ERROR");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+
+        #region Private Methods
+        private static XmlReader GetXmlReader(string path)
+        {
+            var jsonContent = File.ReadAllText(path);
+            var xdoc = JsonConvert.DeserializeXNode(jsonContent);
+            return xdoc.CreateReader();
+        }
+
+        private static void SetLoggerSettings()
+        {
+            var path = String.Empty;
+
+            try
+            {
+                var appPath = PlatformServices.Default.Application.ApplicationBasePath;
+                var files = Directory.GetFiles(appPath, "*.*", SearchOption.TopDirectoryOnly)
+                                     .Where(f => f.ToLowerInvariant().EndsWith("\\app.config") ||
+                                                 f.ToLowerInvariant().EndsWith("\\app.json")).ToList();
+                if (files != null && files.Count > 0)
+                {
+                    if (files.Count > 1)
+                        throw new Exception("Too many logger configs found.");
+
+                    path = files.FirstOrDefault();
+                    var extention = Path.GetExtension(path);
+                    switch (extention)
+                    {
+                        case ".json":
+                            logger.Factory.Configuration = new XmlLoggingConfiguration(GetXmlReader(path), Path.GetFileName(path));
+                            break;
+                        case ".config":
+                            logger.Factory.Configuration = new XmlLoggingConfiguration(path);
+                            break;
+                        default:
+                            throw new Exception($"unknown log format {extention}.");
+                    }
+                }
+                else
+                {
+                    throw new Exception("No logger config loaded.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"The logger setting are invalid!!!\nPlease check the {path} in the app folder.");
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/qlik-engineapi-generator/QlikApiReader.cs
@@ -0,0 +1,126 @@
+namespace QlikApiParser
+{
+    #region Usings
+    using System;
+    using System.IO;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using NLog;
+    using System.Linq;
+    using System.Collections.Generic;
+    using Newtonsoft.Json.Serialization;
+    #endregion
+
+    public class QlikApiReader
+    {
+        #region Logger
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        #endregion
+
+        #region  private methods
+        private T GetValueFromProperty<T>(JObject jObject, string name)
+        {
+            var children = jObject.Children();
+            foreach (var child in children)
+            {
+                var jProperty = child as JProperty;
+                if (jProperty.Name == name)
+                    return jProperty.First.Value<T>();
+            }
+
+            return default(T);
+        }
+        #endregion
+
+        #region public methods
+        public JObject Parse(string qlikApiFile)
+        {
+            try
+            {
+                var content = File.ReadAllText(qlikApiFile);
+                var result = JObject.Parse(content);
+                var definitionsToken = result["definitions"] as JObject;
+                var definitions = new Dictionary<string, EngineObject>();
+                foreach (var child in definitionsToken.Children())
+                {
+                    var jProperty = child as JProperty;
+                    var engineObject = new EngineObject();
+                    definitions.Add(jProperty.Name, engineObject);
+                    logger.Debug($"Object name: {jProperty.Name}");
+                    foreach (var subChild in child.Children())
+                    {
+                        var jObject = subChild as JObject;
+                        engineObject.Type = GetValueFromProperty<string>(jObject, "type");
+                        engineObject.Description = GetValueFromProperty<string>(jObject, "description");
+                        var properties = GetValueFromProperty<JToken>(jObject, "properties");
+                        if (properties == null)
+                            properties = GetValueFromProperty<JToken>(jObject, "items");
+
+                        if (properties == null)
+                            logger.Debug("No properties found. EMPTY");
+                        else
+                        {
+                            foreach (var prop in properties)
+                            {
+                                var jPropProperty = prop as JProperty;
+                                var propName = jPropProperty.Name;
+                                logger.Debug($"Parameter name: {propName}");
+
+                                var parameter = new EngineParameter();
+                                if (propName == "$ref")
+                                {
+                                    parameter.Ref = jPropProperty.First.Value<string>();
+                                    logger.Debug($"REF: {parameter.Ref}");
+                                }
+
+                                var jSubObject = prop.First as JObject;
+                                if (jSubObject != null)
+                                {
+                                    parameter = jSubObject.ToObject<EngineParameter>();
+                                    parameter.Name = propName;
+                                    parameter.Ref = GetValueFromProperty<string>(jSubObject, "$ref");
+                                    if (parameter.Description != null && parameter.Description.Contains("The default value is"))
+                                        parameter.DefaultValueFromDescription = parameter.DefaultValue;
+                                }
+
+                                engineObject.Parameters.Add(parameter);
+                            }
+                        }
+                    }
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "The json could not parse.");
+                return null;
+            }
+        }
+        #endregion
+    }
+
+    #region  Helper Classes
+    public class EngineObject
+    {
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public List<EngineParameter> Parameters { get; set; } = new List<EngineParameter>();
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class EngineParameter
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Format { get; set; }
+        public string Type { get; set; }
+        public string DefaultValue { get; set; }
+        public bool Required { get; set; }
+
+        public string DefaultValueFromDescription { get; set; }
+        public string Ref { get; set; }
+    }
+    #endregion
+}
