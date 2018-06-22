e8fa8f5b35072c1f3bd2834bfa9a57546ead97ad
Martin Berthold <martin.berthold@akquinet.de>
2018-06-22 08:10:10 +0200

    code cleaning

--- src/qlik-engineapi-generator/QlikApiObjects.cs
+++ src/qlik-engineapi-generator/QlikApiObjects.cs
@@ -121,10 +121,7 @@ namespace QlikApiParser
 
     [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
                 NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-    public class EngineResponse : EngineAdvancedTypes 
-    {
-        //???
-    }
+    public class EngineResponse : EngineAdvancedTypes { }
 
     public class EngineInterface : EngineBase, IEngineObject
     {
